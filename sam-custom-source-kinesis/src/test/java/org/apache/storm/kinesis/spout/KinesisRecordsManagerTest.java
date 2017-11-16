16802f57907959272ebeddaec6a54cc5563c7e7f
georgevetticaden <george.vetticaden@gmail.com>
2017-11-16 13:43:10 -0600

    updates

--- sam-custom-source-kinesis/src/test/java/org/apache/storm/kinesis/spout/KinesisRecordsManagerTest.java
+++ sam-custom-source-kinesis/src/test/java/org/apache/storm/kinesis/spout/KinesisRecordsManagerTest.java
@@ -4,8 +4,6 @@ import hortonworks.hdf.sam.custom.source.kinesis.AvroSchemaRegistryRecordToTuple
 
 import java.util.Date;
 
-import org.apache.storm.spout.SpoutOutputCollector;
-import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
